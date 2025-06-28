d87fb6adb16d5c8c3934d6a78b5513b8e88252fa
iVeluu <velualex10@gmail.com>
2025-06-27 18:11:10 -0600

    renaming dbs

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -8,7 +8,7 @@ import {
 } from 'typeorm';
 import { ProductImage } from './product-image-identity';
 
-@Entity()
+@Entity({ name: 'products' })
 export class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
